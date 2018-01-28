c1400230743f02519c143c5486e72b3029612a4d
EeOneDown <rentgen.stalker@yandex.ru>
2018-01-28 21:36:24 +0300

    Travis

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='spbuTimetableAPI',
-      version='0.1.1',
+      version='0.1.2',
       description='Python SPbU TimeTable API.',
       long_description=readme(),
       author='EeOneDown',
